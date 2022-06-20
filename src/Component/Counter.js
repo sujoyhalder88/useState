b92468d5e5abd99630d93e5e7cf3c0fd30851eb5
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-06-20 10:13:04 +0530

    another ways

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -4,24 +4,42 @@ import "./Counter.css";
 const Counter = () => {
     const [count, setCount] = useState(0);
 
-    const increment = () => {
-        setCount(count + 1);
-    };
+    //     const increment = () => {
+    //         setCount(count + 1);
+    //     };
 
-    const Decrement = () => {
-        setCount(count - 1);
-    };
+    //     const Decrement = () => {
+    //         setCount(count - 1);
+    //     };
 
-    const Reset = () => {
-        setCount(0);
-    };
+    //     const Reset = () => {
+    //         setCount(0);
+    //     };
 
     return (
         <div className="text">
             <h1>Count:{count}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={Decrement}>Decrement</button>
-            <button onClick={Reset}>Reset</button>
+            <button
+                onClick={() => {
+                    setCount(count + 1);
+                }}
+            >
+                Increment
+            </button>
+            <button
+                onClick={() => {
+                    setCount(count - 1);
+                }}
+            >
+                Decrement
+            </button>
+            <button
+                onClick={() => {
+                    setCount(0);
+                }}
+            >
+                Reset
+            </button>
         </div>
     );
 };
